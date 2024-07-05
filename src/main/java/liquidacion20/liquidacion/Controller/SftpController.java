864f9e4ee0f6b5217500b6770e8d8dc0dca42812
Joshua Rosales Figueroa <joshuadrf@gmail.com>
2024-07-05 14:47:33 -0600

    commit

--- src/main/java/liquidacion20/liquidacion/Controller/SftpController.java
+++ src/main/java/liquidacion20/liquidacion/Controller/SftpController.java
@@ -266,7 +266,7 @@ public class SftpController {
         Pattern pattern = Pattern.compile(
                 "02(\\d{2})(\\d{16})(\\d{6})(\\d{12})(\\d{10})(\\d{2})(\\d{6})(\\d{4})(\\d{6})(\\d{8})");
 
-    Pattern pattern2 = Pattern.compile("03(\\d{2})(\\d{6})(\\d{12})");
+    Pattern pattern2 = Pattern.compile("03(\\d{2})(\\d{12})");
 
         Matcher matcher = pattern.matcher(fileContent);
         Matcher matcher2 = pattern2.matcher(fileContent);
