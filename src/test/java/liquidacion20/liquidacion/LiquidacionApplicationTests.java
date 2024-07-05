c66fd677a94f50e634482666b482c0157d99e746
Joshua Rosales Figueroa <joshuadrf@gmail.com>
2024-07-05 02:15:44 -0600

    Commit

--- src/test/java/liquidacion20/liquidacion/LiquidacionApplicationTests.java
+++ src/test/java/liquidacion20/liquidacion/LiquidacionApplicationTests.java
@@ -1,10 +1,10 @@
-package Liquidacion.Liquidicacion;
+package liquidacion20.liquidacion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LiquidicacionApplicationTests {
+class LiquidacionApplicationTests {
 
 	@Test
 	void contextLoads() {
